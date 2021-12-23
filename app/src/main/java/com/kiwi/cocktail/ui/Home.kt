7e5125e15c63cbda387e4fd77c79d001a5b8dc37
HvSimon <a309011@gmail.com>
2021-12-24 01:26:45 +0800

    Changed icon

--- app/src/main/java/com/kiwi/cocktail/ui/Home.kt
+++ app/src/main/java/com/kiwi/cocktail/ui/Home.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -21,6 +22,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.insets.navigationBarsPadding
 import com.kiwi.cocktail.AppNavigation
+import com.kiwi.cocktail.R
 import com.kiwi.cocktail.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -46,10 +48,14 @@ fun HomeBottomNavigation(
         val currentDestination = navBackStackEntry?.destination
 
         HomeNavigationItems.forEach { item ->
+            
+            val selected = currentDestination?.hierarchy?.any { it.route == item.route } == true
+            // <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            val iconRes = if(selected) R.drawable.ic_mojito_black else R.drawable.ic_mojito_line
             NavigationBarItem(
-                icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                icon = { Icon(painterResource(id = iconRes), contentDescription = null) },
                 label = { Text(text = stringResource(id = item.labelResId)) },
-                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                selected = selected,
                 onClick = {
                     navController.navigate(item.route) {
                         popUpTo(navController.graph.findStartDestination().id) { saveState = true }
