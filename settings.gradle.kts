3fb7711f3a73876fe629ae1b33f3f60842c3ed69
HvSimon <a309011@gmail.com>
2022-02-08 22:26:56 +0800

    Add todo comment to remind remove VERSION_CATALOGS flag

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+// TODO: Remove below. Version catalogs is promoted to a stable feature in 7.4
 enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
