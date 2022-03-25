4d8224fc5264ff8828197370121bf8179a7caea4
HvSimon <a309011@gmail.com>
2022-03-25 20:18:16 +0800

    Upgrade AboutLibraries to 10.0.1

--- ui-about/src/main/java/com/kiwi/ui_about/Licenses.kt
+++ ui-about/src/main/java/com/kiwi/ui_about/Licenses.kt
@@ -1,16 +1,13 @@
 package com.kiwi.ui_about
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.lightColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
@@ -22,32 +19,37 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Licenses(
     navigateUp: () -> Unit,
 ) {
-    MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
 
-        val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
+    val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
 
-        Scaffold(
-            topBar = {
-                TopBar(
-                    navigateUp = navigateUp,
-                    scrollBehavior = scrollBehavior,
-                )
-            },
-            modifier = Modifier
-                .fillMaxSize()
-                .statusBarsPadding()
-                .nestedScroll(scrollBehavior.nestedScrollConnection)
-        ) {
-            LibrariesContainer(
-                modifier = Modifier.fillMaxSize()
+    Scaffold(
+        topBar = {
+            TopBar(
+                navigateUp = navigateUp,
+                scrollBehavior = scrollBehavior,
             )
-        }
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) {
+        LibrariesContainer(
+            colors = LibraryDefaults.libraryColors(
+                backgroundColor = MaterialTheme.colorScheme.background,
+                contentColor = MaterialTheme.colorScheme.onBackground,
+                badgeBackgroundColor = MaterialTheme.colorScheme.primary,
+                badgeContentColor = MaterialTheme.colorScheme.onPrimary,
+            ),
+            modifier = Modifier.fillMaxSize(),
+        )
     }
 }
 
