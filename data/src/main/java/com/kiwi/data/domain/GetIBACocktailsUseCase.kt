ec4d6003d2e3c1c526ee6bfbae96852a978fd241
HvSimon <a309011@gmail.com>
2022-03-27 16:06:27 +0800

    Use Store to cache base liquors and iba cocktails

--- data/src/main/java/com/kiwi/data/domain/GetIBACocktailsUseCase.kt
+++ data/src/main/java/com/kiwi/data/domain/GetIBACocktailsUseCase.kt
@@ -4,21 +4,21 @@ import com.dropbox.android.external.store4.Store
 import com.dropbox.android.external.store4.get
 import com.kiwi.data.entities.FullDrinkEntity
 import com.kiwi.data.entities.IBACategoryType
+import com.kiwi.data.entities.IBACocktail
 import com.kiwi.data.entities.SimpleDrinkDto
-import com.kiwi.data.repositories.CocktailRepository
 import javax.inject.Inject
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 
 class GetIBACocktailsUseCase @Inject constructor(
-    private val cocktailRepository: CocktailRepository,
+    private val ibaCocktailsStore: Store<Unit, List<IBACocktail>>,
     private val cocktailStore: Store<String, FullDrinkEntity>,
 ) {
 
     suspend operator fun invoke(type: IBACategoryType): Result<List<SimpleDrinkDto>> = runCatching {
         coroutineScope {
-            cocktailRepository.getIBACocktails()
+            ibaCocktailsStore.get(Unit)
                 .filter { it.iba == type && it.id.isNotEmpty() }
                 .map { async { cocktailStore.get(it.id) } }
                 .awaitAll()
