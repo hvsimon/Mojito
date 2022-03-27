a28fc71a8aa653b010bd47576568ecc4d7438074
HvSimon <a309011@gmail.com>
2022-03-27 16:17:37 +0800

    Version bump to 0.3.0(5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.kiwi.cocktail"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 4
+        versionCode = 5
         versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
