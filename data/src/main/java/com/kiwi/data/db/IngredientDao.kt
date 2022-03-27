13078ad1d2ed1b9d58311e641270da7d0b02871d
HvSimon <a309011@gmail.com>
2022-03-28 00:46:29 +0800

    Query ingredient with COLLATE NOCASE for case-insensitive

--- data/src/main/java/com/kiwi/data/db/IngredientDao.kt
+++ data/src/main/java/com/kiwi/data/db/IngredientDao.kt
@@ -9,7 +9,11 @@ import com.kiwi.data.entities.FullIngredientEntity
 @Dao
 interface IngredientDao {
 
-    @Query("SELECT * FROM FullIngredientEntity WHERE strIngredient = :ingredientName")
+    @Query(
+        """
+        SELECT * FROM FullIngredientEntity WHERE strIngredient = :ingredientName COLLATE NOCASE
+    """
+    )
     suspend fun getIngredientByName(ingredientName: String): FullIngredientEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
