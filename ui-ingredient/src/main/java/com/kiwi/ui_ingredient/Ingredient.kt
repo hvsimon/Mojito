f4fc2bbbf6416b2fa26c8706871e06f6f7f3a74d
HvSimon <a309011@gmail.com>
2022-03-27 22:58:13 +0800

    Enables text selection for ingredient desc and recipe steps

--- ui-ingredient/src/main/java/com/kiwi/ui_ingredient/Ingredient.kt
+++ ui-ingredient/src/main/java/com/kiwi/ui_ingredient/Ingredient.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -124,8 +125,10 @@ private fun IngredientImage(imageUrl: String) {
 
 @Composable
 private fun IngredientDesc(desc: String) {
-    Text(
-        text = desc,
-        modifier = Modifier.fillMaxWidth(),
-    )
+    SelectionContainer {
+        Text(
+            text = desc,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
 }
--- ui-recipe/src/main/java/com/kiwi/ui_recipe/Recipe.kt
+++ ui-recipe/src/main/java/com/kiwi/ui_recipe/Recipe.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -204,10 +205,12 @@ private fun Step(
             text = stringResource(id = R.string.step),
             style = MaterialTheme.typography.headlineLarge,
         )
-        Text(
-            text = steps,
-            style = MaterialTheme.typography.bodyLarge,
-        )
+        SelectionContainer {
+            Text(
+                text = steps,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        }
     }
 }
 
