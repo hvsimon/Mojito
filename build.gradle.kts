d455bc7a5f7353f98d3f56138abf7eb9a13d30b9
HvSimon <a309011@gmail.com>
2022-04-15 00:05:08 +0800

    Use version catalog to managing gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:10.0.0")
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
+        classpath(libs.android.pluginGradle)
+        classpath(libs.kotlin.pluginGradle)
+        classpath(libs.hilt.pluginGradle)
+        classpath(libs.aboutlibraries.pluginGradle)
+        classpath(libs.googleservice.pluginGradle)
+        classpath(libs.crashlytics.pluginGradle)
     }
 }
 
